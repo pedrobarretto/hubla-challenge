b822255785f9ba8b936047a6178d5859249acbe0
pedrobarretto <pedro@barretto.com.br>
2023-10-08 17:50:48 -0300

    feat: create swagger docs for sales controller

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Hubla API')
+    .setDescription('Hubla API for sellers and afiliates make their sales!')
+    .setVersion('1.0')
+    .addTag('sellers')
+    .addTag('afiliates')
+    .addTag('sells')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors();
   await app.listen(8000);
 }
